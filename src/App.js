86084a2dd9660491973706baf0760f0f4c1fdc3b
Ahmed Musawir <moose@Ahmeds-Mac.local>
2023-03-30 10:17:42 +0800

    30Mar2023 - setup done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className=''>
+      <h1>You may begin...</h1>
+    </div>
+  );
+};
+
+export default App;
